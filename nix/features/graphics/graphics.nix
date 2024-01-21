f76fde81339b3c535468a441e3c52d81c85c57c5
CryoMyst <CryoMyst@hotmail.com>
2024-01-21 13:52:50 +1000

    Fix up gpu nix

--- nix/features/graphics/graphics.nix
+++ nix/features/graphics/graphics.nix
@@ -13,42 +13,35 @@ in {
     };
   };
 
-  config = mkMerge [
-    (mkIf cfg.enable {})
-    (mkIf cfg.gpu == "amdgpu" {
-      boot.initrd.kernelModules = [ "amdgpu" ];
-      services.xserver.videoDrivers = [ "amdgpu" ];
-      hardware = {
-        opengl = {
-          enable = true;
-          extraPackages = with pkgs; [ rocm-opencl-icd rocm-opencl-runtime amdvlk mesa.drivers ];
-          extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
-          driSupport = true;
-          driSupport32Bit = true;
-        };
+  config = mkIf cfg.enable {
+    boot.initrd.kernelModules = if cfg.gpu == "amdgpu" then [ "amdgpu" ] else [];
+    services.xserver.videoDrivers = if cfg.gpu == "amdgpu" then [ "amdgpu" ] else if cfg.gpu == "nvidia" then [ "nvidia" ] else [];
+
+    hardware = if cfg.gpu == "amdgpu" then {
+      opengl = {
+        enable = true;
+        extraPackages = with pkgs; [ rocm-opencl-icd rocm-opencl-runtime amdvlk mesa.drivers ];
+        extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
+        driSupport = true;
+        driSupport32Bit = true;
       };
-    })
-    (mkIf cfg.gpu == "apple" {
-      hardware = {
-        asahi = {
-          addEdgeKernelConfig = true;
-          useExperimentalGPUDriver = true;
-          experimentalGPUInstallMode = "overlay";
-          withRust = true;
-        };
-        opengl.enable = true;
+    } else if cfg.gpu == "apple" then {
+      asahi = {
+        addEdgeKernelConfig = true;
+        useExperimentalGPUDriver = true;
+        experimentalGPUInstallMode = "overlay";
+        withRust = true;
       };
-    })
-    (mkIf cfg.gpu == "nvidia" {
-      services.xserver.videoDrivers = [ "nvidia" ];
-      hardware = {
-        opengl = {
-          enable = true;
-          driSupport = true;
-          driSupport32Bit = true;
-          package = config.boot.kernelPackages.nvidiaPackages.beta;
-        };
+      opengl.enable = true;
+    } else if cfg.gpu == "nvidia" then {
+      opengl = {
+        enable = true;
+        driSupport = true;
+        driSupport32Bit = true;
       };
-    })
-  ];
+      nvidia = {
+        package = config.boot.kernelPackages.nvidiaPackages.beta;
+      };
+    } else {};
+  };
 }
